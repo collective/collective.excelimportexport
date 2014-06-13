c545d0c85485409697b45e5a72d897b1b2f39650
Ross Patterson <me@rpatterson.net>
2014-06-13 10:28:31 -0700

    `templer plone_basic` output.

---
+++ src/collective/excelimportexport/testing.py
@@ -0,0 +1,41 @@
+from plone.app.testing import PloneSandboxLayer
+from plone.app.testing import applyProfile
+from plone.app.testing import PLONE_FIXTURE
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import FunctionalTesting
+
+from plone.testing import z2
+
+from zope.configuration import xmlconfig
+
+
+class CollectiveexcelimportexportLayer(PloneSandboxLayer):
+
+    defaultBases = (PLONE_FIXTURE,)
+
+    def setUpZope(self, app, configurationContext):
+        # Load ZCML
+        import collective.excelimportexport
+        xmlconfig.file(
+            'configure.zcml',
+            collective.excelimportexport,
+            context=configurationContext
+        )
+
+        # Install products that use an old-style initialize() function
+        #z2.installProduct(app, 'Products.PloneFormGen')
+
+#    def tearDownZope(self, app):
+#        # Uninstall products installed above
+#        z2.uninstallProduct(app, 'Products.PloneFormGen')
+
+
+COLLECTIVE_EXCELIMPORTEXPORT_FIXTURE = CollectiveexcelimportexportLayer()
+COLLECTIVE_EXCELIMPORTEXPORT_INTEGRATION_TESTING = IntegrationTesting(
+    bases=(COLLECTIVE_EXCELIMPORTEXPORT_FIXTURE,),
+    name="CollectiveexcelimportexportLayer:Integration"
+)
+COLLECTIVE_EXCELIMPORTEXPORT_FUNCTIONAL_TESTING = FunctionalTesting(
+    bases=(COLLECTIVE_EXCELIMPORTEXPORT_FIXTURE, z2.ZSERVER_FIXTURE),
+    name="CollectiveexcelimportexportLayer:Functional"
+)
